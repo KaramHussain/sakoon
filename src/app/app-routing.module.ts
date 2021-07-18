e4d820fdf89815648a574a0e5c40accd45cd0470
unknown <KaramHussain>
2021-07-18 07:29:03 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginFormComponent } from './feature/login/login-form/login-form.component';
+const routes: Routes = [
+  {path:'login',component:LoginFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
